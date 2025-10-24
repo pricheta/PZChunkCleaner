48214441942e2b1ee6dd371418b7b5c37e3e9550
pricheta <pricheta@inbox.ru>
2025-10-24 22:08:54 +0800

    Работа кипит

--- adapters/chunk_fetcher/windows11.py
+++ adapters/chunk_fetcher/windows11.py
@@ -9,8 +9,20 @@ from domain.ports.chunk_fetcher import ChunkFetcher
 class Windows11ChunkFetcher(ChunkFetcher):
     def __init__(
         self,
+        directory: Path,
     ) -> None:
-        ...
+        self.directory = directory
 
     def fetch(self) -> list[Chunk]:
-        ...
+        chunks = []
+
+        for file in self.directory.iterdir():
+            name = str(file.name).removesuffix(".bin")
+            parts = name.split("_")
+
+            x = int(parts[1])
+            y = int(parts[2])
+
+            chunks.append(Chunk(x_coordinate=x, y_coordinate=y))
+
+        return chunks
