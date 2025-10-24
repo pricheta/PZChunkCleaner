c1ed0a39d6d9bc440fab875487bd05557b31fa07
pricheta <pricheta@inbox.ru>
2025-10-25 01:19:29 +0800

    Работа кипит

--- adapters/chunk_deleter/windows11.py
+++ adapters/chunk_deleter/windows11.py
@@ -13,15 +13,17 @@ class Windows11ChunkDeleter(ChunkDeleter):
         self,
         directory: Path,
         vehicle_db_session: Session,
+        save_files_dir_name: str = "map",
         filename_template: str = "map_{}_{}.bin",
     ) -> None:
         self.directory = directory
         self.vehicle_db_session = vehicle_db_session
+        self.save_files_dir_name = save_files_dir_name
         self.filename_template = filename_template
 
     def delete(self, chunk: Chunk) -> None:
         filename = self.filename_template.format(chunk.x_coordinate, chunk.y_coordinate)
-        (self.directory / filename).unlink()
+        (self.directory / self.save_files_dir_name / filename).unlink()
 
         self.vehicle_db_session.query(Vehicle).filter(
             and_(Vehicle.wx == chunk.x_coordinate, Vehicle.wy == chunk.y_coordinate)
