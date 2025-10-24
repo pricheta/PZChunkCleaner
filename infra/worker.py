1ec6d11853041791690cbfaf1bfab7d74ca60999
pricheta <pricheta@inbox.ru>
2025-10-24 22:47:58 +0800

    Работа кипит

---
+++ infra/worker.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from time import sleep
+from typing import Callable
+
+datetime_format = "%Y-%m-%dT%H-%M-%S"
+get_now = lambda: datetime.now().strftime(datetime_format)
+
+
+class Worker:
+    def __init__(
+        self,
+        func: Callable,
+        args = None,
+        kwargs = None,
+        immediate_run: bool = True,
+        seconds_between_runs: int = 60,
+    ) -> None:
+        self.func = func
+        self.args = args or []
+        self.kwargs = kwargs or {}
+        self.immediate_run = immediate_run
+        self.seconds_between_runs = seconds_between_runs
+
+    def run(self) -> None:
+        if not self.immediate_run:
+            sleep(self.seconds_between_runs)
+
+        while True:
+            print(f'Calling {self.func.__name__} at {get_now()}')
+            self.func(*self.args, **self.kwargs)
+            sleep(self.seconds_between_runs)
