91055ec3d4b26c0686095dd3a1e6ae702ed0f83b
pricheta <pricheta@inbox.ru>
2025-10-24 22:24:33 +0800

    Работа кипит

---
+++ adapters/chunk_deleter/windows11.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+from sqlalchemy import and_
+from sqlalchemy.orm import Session
+
+from db import Vehicle
+from domain.entities import Chunk
+from domain.ports.chunk_deleter import ChunkDeleter
+
+
+class Windows11ChunkDeleter(ChunkDeleter):
+    def __init__(
+        self,
+        directory: Path,
+        vehicle_db_session: Session,
+        filename_template: str = 'map_{}_{}.bin',
+    ) -> None:
+        self.directory = directory
+        self.vehicle_db_session = vehicle_db_session
+        self.filename_template = filename_template
+
+    def delete(self, chunk: Chunk) -> None:
+        filename = self.filename_template.format(chunk.x_coordinate, chunk.y_coordinate)
+        (self.directory / filename).unlink()
+
+        self.vehicle_db_session.query(Vehicle).filter(
+            and_(
+                Vehicle.wx == chunk.x_coordinate,
+                Vehicle.wy == chunk.y_coordinate
+            )
+        ).delete(synchronize_session=False)
+        self.vehicle_db_session.commit()
+
+
+
---
+++ infra/vehicle_db.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+from pathlib import Path
+
+from sqlalchemy import Column, Integer, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+Base = declarative_base()
+
+
+class Vehicle(Base):
+    __tablename__ = "vehicles"
+
+    id = Column(Integer, primary_key=True)
+    wx = Column(Integer)
+    wy = Column(Integer)
+
+
+@lru_cache(maxsize=1)
+def get_vehicle_db_session(db_path: Path):
+    engine_str = 'sqlite:///' + str(db_path)
+    engine = create_engine(engine_str, echo=True)
+    Session = sessionmaker(bind=engine)
+    return Session()
