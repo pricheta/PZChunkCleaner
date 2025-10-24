b33d4fcf45d11289c5b685b4e543f2a0d14b3188
pricheta <pricheta@inbox.ru>
2025-10-24 22:30:58 +0800

    Работа кипит

--- domain/core/chunk_cleaner.py
+++ domain/core/chunk_cleaner.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 
 from domain.core.config import ChunkCleanerConfig
-from domain.entities import Chunk
 from domain.ports.backuper import Backuper
 from domain.ports.chunk_deleter import ChunkDeleter
 from domain.ports.chunk_fetcher import ChunkFetcher
@@ -37,40 +36,3 @@ class ChunkCleaner:
             if chunk.created_at <= threshold_datetime:
                 continue
             self.chunk_deleter.delete(chunk)
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    if MAKE_BACKUP_FEATURE_FLAG:
-        make_directory_backup(SAVE_DIRS_PATH / SAVE_DIR_NAME)
-
-    if CLEAR_CHUNKS_FEATURE_FLAG:
-        for file in (SAVE_DIRS_PATH / SAVE_DIR_NAME / CHUNK_DATA_DIR).iterdir():
-            chunk = Chunk.build_from_filename(str(file.name))
-
-            if chunk not in SAVE_CHUNK_AREA:
-                (SAVE_DIRS_PATH / SAVE_DIR_NAME / CHUNK_DATA_DIR / file).unlink()
-
-    if CLEAR_CARS_FEATURE_FLAG:
-        try:
-            session = get_session(SAVE_DIRS_PATH / SAVE_DIR_NAME / VEHICLES_DB_NAME)
-            vehicles = session.query(Vehicle).all()
-
-            for vehicle in vehicles:
-                chunk = Chunk(x_coordinate=vehicle.wx, y_coordinate=vehicle.wy)
-
-                if chunk not in SAVE_CHUNK_AREA:
-                    session.delete(vehicle)
-            session.commit()
-        except:
-            print('Возникла ошибка при очистке автомобилей')
