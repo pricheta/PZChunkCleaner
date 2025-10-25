6be4faf437aa6a1da542581f9ad0a9d2fbc30328
pricheta <pricheta@inbox.ru>
2025-10-25 22:22:47 +0800

    Работа кипит

--- domain/core/config.py
+++ domain/core/config.py
@@ -5,7 +5,7 @@ from pydantic.v1 import BaseSettings
 
 class ChunkCleanerConfig(BaseSettings):
     MAKE_BACKUP_FEATURE_FLAG: bool = True
-    MAX_CHUNK_AGE_MINUTES: int = 30
+    MAX_CHUNK_AGE_MINUTES: int = 0
     SAVE_FILE_DIR: Path = Path("C:/Users/pricheta/Zomboid/Saves/Sandbox/v5")
     IMMEDIATE_RUN: bool = True
     SECONDS_BETWEEN_RUNS: int = 60 * 30
